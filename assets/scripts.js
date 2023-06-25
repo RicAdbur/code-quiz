04f9dc825c9df7773a36f01caa121968afa8e5ba
Max <maxrrice@hotmail.com>
2023-06-24 20:15:24 -0500

    more basic CSS, minor HTML tweaks, and some beginning javascript variables

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,56 @@
+// Selecting HTML elements and storing them as variables
+var highScoreLink = document.getElementById("high-score-link");
+var timer = document.getElementById("timer");
+var startButton = document.getElementById("start-button");
+var scoreDisplay = document.getElementById("score-display");
+var inputInitials = document.getElementById("initials");
+var submitButton = document.getElementById("submit-score-button");
+var feedback = document.getElementById("feedback")
+
+
+// Defining other global variables
+var score = 0;
+
+
+
+// Each question will be stored as an object inside an array
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+
+];
+
+
+
+
+
+
+
+
+
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
 // WHEN I answer a question
